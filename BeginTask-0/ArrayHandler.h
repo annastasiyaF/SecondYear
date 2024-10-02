e5141f7f14a65447c9e9cab68f72865c8b426ee3
annastasiyaF <184filippova8a@mail.ru>
2024-10-02 13:59:31 +0300

    fff

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -2,15 +2,42 @@
 template<typename T>
 class ArrayHandler {
 private:
-    size_t _size;
+    int8_t _max;
+    int8_t _min;
+    int8_t _size;
+    size_t _max_size;
     T* _array;
-public:
-    ArrayHandler(size_t size = 10) {
+    T* _id_array;
+    size_t _count;
 
+public:
+    ArrayHandler(int size = 10){
+        _size = size;
+        _id_array = new int8_t[_max_size];
+        _max = INT32_MIN;
+        _min = INT32_MAX;
+        _count = 0;
+        for (int i = 0; i < 10 ;i++){
+            _id_array[i] = -1;
+        }
     }
 
-    void AppendElem(T elem) {
-
+    void Append(int8_t new_element){
+        if (new_element > _max){
+            _max = new_element;
+        }
+        if (new_element < _min){
+            _min = new_element;
+        }
+        if (_count == _max_size) {
+            _max_size += 10;
+            int8_t* new_arr = new int8_t[_max_size];
+            std::memcpy(new_arr, _array, _count*sizeof(int8_t));
+            delete [] _array;
+            _array = new_arr;
+        }
+        _array[_count] = new_element;
+        _count++;
     }
 
     bool IsContains(T elem) {
@@ -18,15 +45,23 @@ public:
     }
 
     T GetMax() {
-        return 0;
+        for (int i = 0; i < _count; i++) {
+            if (_max < _array[i])
+                _max = _array[i];
+        }
+        return _max;
     }
 
     T GetMin() {
-        return 0;
+        for (int i = 0; i < _count; i++) {
+            if (_min > _array[i])
+                _min = _array[i];
+        }
+        return _min;
     }
 
     ~ArrayHandler() {
-
+        delete [] _array;
     }
 
 };
